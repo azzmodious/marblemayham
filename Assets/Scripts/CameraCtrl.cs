d9a23bab5e9d663418dc1552ad3a03e2b02d73df
Max <max@ge.com>
2016-03-02 23:08:38 -0500

    Add Ball and Camera code

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCtrl : MonoBehaviour {
+	private GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+	public float speed = 5.0f;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	void ProcessInput(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
