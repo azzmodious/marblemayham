5f228b5388db4c145192bf28d89530b60f867429
Max <max@ge.com>
2016-03-03 00:49:56 -0500

    GameCtrl added

---
+++ Assets/Scripts/GameCtrl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void PlayerCollisionHandler(GameObject player, Collider other);
+public class GameCtrl : MonoBehaviour {
+
+	private static GameCtrl gameCtrl; 
+
+	public static event PlayerCollisionHandler onPlayerCollision; 
+	private static GameCtrl instance{
+		get{
+			if (!gameCtrl) {
+				gameCtrl = FindObjectOfType (typeof(GameCtrl)) as GameCtrl;
+
+				if (!gameCtrl) {
+					Debug.LogError ("There needs to be an active GameCtrl script on a game object");
+				} else {
+					gameCtrl.Init();
+				}
+			}
+			return gameCtrl;
+		}
+	}
+
+	void Init(){
+		onPlayerCollision += handlePlayerCollision;
+	}
+
+	public static void TriggerPlayerCollisionEvent(GameObject player, Collider other){
+		onPlayerCollision(player, other);
+	}
+
+	private void handlePlayerCollision(GameObject player, Collider other){
+		if (other.gameObject.CompareTag ("pickup")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		GameCtrl.instance.Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -7,6 +7,7 @@ public class PlayerCtrl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
@@ -17,9 +18,12 @@ public class PlayerCtrl : MonoBehaviour {
 	void ProcessInput(){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		GameCtrl.TriggerPlayerCollisionEvent (gameObject, other);
+
 	}
 }
